b2c5781be16fc82273bf5f412b2daef68fd82325
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-21 19:32:52 +0530

    added the balance functionality

--- api/models/transactionModel.js
+++ api/models/transactionModel.js
@@ -7,7 +7,7 @@ const TransactionSchema = new mongoose.Schema(
       required: true,
     },
     price: {
-      type: String,
+      type: Number,
       required: true,
     },
     description: {
