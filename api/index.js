6f0bb2f3ead8fb9699375fbb9136cd11f40ef56a
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-04-23 20:13:31 +0530

    used port from .env or hardcoded 3000 rather than just hardcoded 3000

--- api/index.js
+++ api/index.js
@@ -13,6 +13,6 @@ app.use(express.json());
 
 app.use("/api", require("./routes/transactionRoute"));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
