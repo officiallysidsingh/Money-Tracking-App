2637c632f71f34936e92881d137603a9a213a0b6
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-05-04 23:29:52 +0530

    added ege cases

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/dbConnection");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
+const transactionRoute = require("./routes/transactionRoute");
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,8 +12,12 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("/api", require("./routes/transactionRoute"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api", transactionRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
